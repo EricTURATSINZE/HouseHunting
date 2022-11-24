1001d3df5cd5a840e5aecb7fe5fb56dcc36897be
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2022-11-24 09:57:44 +0200

    fixed bugs and added navigations through screens

---
+++ app/src/main/java/com/example/househunting/HouseDetail.java
@@ -0,0 +1,15 @@
+package com.example.househunting;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HouseDetail extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.house_activity);
+
+    }
+}
---
+++ app/src/main/java/com/example/househunting/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.househunting;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.househunting.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreen.this, SignUp.class));
+
+            }
+        }, 2000L);
+
+
+    }
+
+}
