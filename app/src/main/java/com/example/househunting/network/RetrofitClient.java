8bdd51e0adc93e82fb69f89fde7597f0b27f2d9a
unknown <daveleo250@gmail.com>
2022-12-09 11:23:12 +0200

    Ip address connection

--- app/src/main/java/com/example/househunting/network/RetrofitClient.java
+++ app/src/main/java/com/example/househunting/network/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
     private static Retrofit retrofit;
 //    private static final String BASE_URL = "https://house-hunting.onrender.com/api/v1/";
-    private static final String BASE_URL = "http://192.168.1.64:5000/api/v1/";
+    private static final String BASE_URL = "http://172.29.105.140:5000/api/v1/";
 
     public static Retrofit getClient(String url) {
         if(url.equals("")) {
