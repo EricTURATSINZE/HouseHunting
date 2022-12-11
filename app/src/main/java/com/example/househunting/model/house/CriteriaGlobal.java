7896388b9a4a5e6435092a933581a7628a25018c
unknown <daveleo250@gmail.com>
2022-12-11 08:09:32 +0200

    Fixing search filter error

--- app/src/main/java/com/example/househunting/model/house/CriteriaGlobal.java
+++ app/src/main/java/com/example/househunting/model/house/CriteriaGlobal.java
@@ -1,5 +1,9 @@
 package com.example.househunting.model.house;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -21,10 +25,6 @@ public class CriteriaGlobal implements Criteria
             if(Integer.toString(data.getBedRooms()).equals(searchString) || data.getLocation().getAddress().toLowerCase(Locale.ROOT).contains(searchString))
             {
                 houseList.add(data);
-            }
-            else
-            {
-                return null;
             }
         }
         return houseList;
